a92ce3817c6ce2a14dcf1ef8daf0bb2adb552182
melinabcheca <melina.checa@gmail.com>
2022-11-06 21:30:49 -0300

    Agrego Formulario JS

---
+++ js/form.js
@@ -0,0 +1,48 @@
+const btnResumen = document.getElementById("btnResumen");
+const pResultado = document.getElementById("resultado");
+
+const valorEntradaGeneral = 200;
+const valorEstudiante = 0.2; 
+const valorTrainee = 0.5; 
+const valorJunior = 0.85; 
+let totalPagar = '';
+let cantidad = '';
+let categoria = '';
+
+function obtenerCantidad () {
+    let c = document.getElementById("inputCantidad").value;
+    return c
+}
+
+function obtenerCategoria()  {
+    let c = document.getElementById("sel1").value;
+    return c;
+}
+
+
+function calcularPrecio(){
+    if (obtenerCantidad() == 0) {
+        alert("La cantidad no puede ser cero");
+    } else {
+        if (obtenerCategoria() == "Estudiante"){
+            totalPagar = valorEntradaGeneral * obtenerCantidad() * valorEstudiante;
+        } else if (obtenerCategoria() == "Trainee") {
+            totalPagar = valorEntradaGeneral * obtenerCantidad() * valorTrainee;
+        } else {
+            totalPagar = valorEntradaGeneral * obtenerCantidad() * valorJunior;
+        }
+    }
+    return totalPagar;
+}
+
+
+function resumen(e) {
+    e.preventDefault();
+    console.log("el valor de inputCantidad es: " + obtenerCantidad())
+    console.log("el valor de inputCategoria es: " + obtenerCategoria()) 
+    totalPagar = calcularPrecio();
+    pResultado.innerHTML = `${totalPagar}`;
+
+}
+
+btnResumen.onclick = resumen;
